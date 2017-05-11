e253c783273b08701a1b4769a32377ef2ca3b6ee
robotscissors <cjbazin@gmail.com>
2017-05-10 23:02:50 -0700

    Able to CRUD.

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,3 +1,9 @@
 class Wiki < ActiveRecord::Base
   belongs_to :user
+
+
+  validates :title, length: {minimum: 5}, presence: true
+  validates :body, length: {minimum: 20}, presence: true
+  validates :private, default: false
+  validates :user, presence: true
 end
