a0d1112d9960d0d134d33d6f32bd307d0cd5ddd4
robotscissors <cjbazin@gmail.com>
2017-05-15 19:01:44 -0700

    Added seeding data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# require 'random_data'
+@testUser=User.new(
+  email:  "cjbazin@gmail.com",
+  #encrypted_password:   Faker::Internet.password(8)
+  password: "helloworld",
+  password_confirmation: "helloworld"
+)
+@testUser.skip_confirmation!
+@testUser.save!
+
+
+10.times do
+  @pass = Faker::Internet.password(8)
+  user = User.new(
+    email:  Faker::Internet.email,
+    #encrypted_password:   Faker::Internet.password(8)
+    password: @pass,
+    password_confirmation: @pass
+  )
+  user.skip_confirmation!
+  user.save!
+end
+users = User.all
+
+50.times do #create wikis
+
+ Wiki.create!(
+     title:  Faker::Lorem.sentence(3, true, 4),
+     body:   Faker::Lorem.paragraph(10, true),
+     private: Faker::Boolean.boolean(0.5),
+     user: users.sample
+ )
+end
+wikis = Wiki.all
+
+
+puts "Seed finished"
+puts "#{User.count} Users created"
+puts "#{Wiki.count} Wikis created"
+ # puts "#{Comment.count} comments created"
